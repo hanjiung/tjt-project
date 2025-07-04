aca3f28683f5b8188a2bd8d038381e06ed146f4b
Taeho1218 <true3d.j@gmail.com>
2025-07-05 01:22:09 +0900

    mainchange

--- main.dart
+++ main.dart
@@ -1,17 +1,101 @@
 import 'package:flutter/material.dart';
-
+import 'postlist.dart';
+import 'register.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(const Login());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Login extends StatefulWidget{
+  const login({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Hello, World!'))),
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login>{
+  TextEditingController _idController = TextEditingController();
+  TextEditingController _pwController = TextEditingController();
+  Future<void> sendLoginData() async {
+    Map <String, String> loginData = {
+      'user_id' : _idController.text,
+      'user_pw': _pwController.text,
+    };
+  }
+  @override
+  Widget build(BuildContext){
+    return Scaffold(
+    
+      body: Container(
+        child: Column(
+          mainAxisAlignment: mainAxisAlignment.center,
+          children: [
+            Image.asset(icon),
+            Text(
+              "Log In"
+            ),
+            Container(
+              child:Row(
+                children:[
+                   Column(
+                    children:[
+                      Row(
+                        children:[
+                          Text("ID"),
+                          TextField(
+                            decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: '아이디를 입력하세요'
+                            ),
+                          controller:_idController,
+                          ),
+                          ]
+                        ),
+                    Row(
+                      children:[
+                        Text("PW"),
+                        TextField(
+                          decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: '비밀번호를 입력하세요'
+                        ),
+                        obscureText: true,
+                        controller:_pwController,
+                        ),
+                      ]
+                    ),
+                ]
+                ),
+                  ElevatedButton(
+                    onPressed:(){
+                      // 아이디와 비밀 번호 비교후 리스트 피이지로 이동, 제이슨 객체형태로 보냄. 
+                      Navigator.push(context,MaterialPageRoute(builder:(context)=> PostList()),);
+                    },
+                    child: Text('로그인'),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children:[
+                ElevatedButton(
+                onPressed:(){
+                  //회원 가입 페이지로 이동
+                  Navigator.push(context,MaterialPageRoute(builder(context)=> Register()),);
+                  
+                },
+                child: Text('회원가입'),
+              ),
+              //   ElevatedButton(
+              //   onPressed:(){},
+              //   child: Text('비밀번호를 잊어버렸습니다.'),
+              // ),
+              ],
+
+            ),
+          ],
+        ),
+      ) ,
     );
   }
 }
+
