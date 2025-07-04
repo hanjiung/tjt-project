e5be7086e46a7576f7a83c729bf3f66b20e2898c
Taeho1218 <true3d.j@gmail.com>
2025-07-04 12:04:51 +0900

    Update login.dart

--- login.dart
+++ login.dart
@@ -7,11 +7,30 @@ class login extends StatelessWidget{
     
       body: Container(
         child: Column(
+          mainAxisAlignment: mainAxisAlignment.center,
           children: [
             Image.asset(icon),
             Text(
               "Log In"
             ),
+            Container(
+              child: Column(
+                children:[
+                  Row(
+                    children:[
+                      Text("ID"),
+                      TextField(""),
+                    ]
+                  ),
+                  Row(
+                    children:[
+                      Text("ID"),
+                      TextField(""),
+                    ]
+                  )
+                ]
+              )
+            )
           ],
 
         ),
