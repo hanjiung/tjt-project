63e7f869786bf6f215b204cd3f10615754b88f6e
Taeho1218 <true3d.j@gmail.com>
2025-07-04 14:33:36 +0900

    Update login.dart

--- login.dart
+++ login.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
-class login extends StatelessWidget{
+class Login extends StatefulWidget{
+  const login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login>{
+  final Controller = TextEditingController()
   @override
   Widget build(BuildContext){
     return Scaffold(
@@ -14,28 +22,57 @@ class login extends StatelessWidget{
               "Log In"
             ),
             Container(
-              child: Column(
+              child:Row(
                 children:[
-                  Row(
+                   Column(
                     children:[
-                      Text("ID"),
-                      TextField(""),
-                    ]
+                      Row(
+                        children:[
+                          Text("ID"),
+                          TextField(
+                            decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: '아이디를 입력하세요s'
+                            ),
+                          controller:Controller,),
+                          ];
+                        ),
+                    Row(
+                      children:[
+                        Text("ID"),
+                        TextField(
+                          decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: '아이디를 입력하세요s'
+                        ),
+                        controller:Controller,),
+                      ];
+                    ),
+                ];
+                ),
+                  ElevatedButton(
+                    onPressed:(){},
+                    child: Text('로그인'),
                   ),
-                  Row(
-                    children:[
-                      Text("ID"),
-                      TextField(""),
-                    ]
-                  )
-                ]
-              )
-            )
-          ],
+                ],
+              ),
+            ),
+            Row(
+              children:[
+                ElevatedButton(
+                onPressed(){},
+                child: Text('회원가입'),
+              ),
+                ElevatedButton(
+                onPressed(){},
+                child: Text('비밀번호를 잊어버렸습니다.'),
+              ),
+              ],
 
+            ),
+          ],
         ),
       ) ,
-
     );
   }
 }
